d14e1bb48ef576917c564889bbf7a503f086f7c9
BGope025 <bgope025@gmail.com>
2025-06-23 22:11:46 +0530

    adding notes question

---
+++ TreeDataStructure/BinaryTree/BinarySearchTree.java
@@ -0,0 +1,12 @@
+public class BinarySearchTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+    }
+    public static void main(String[] args) {
+        int value[] = {5, 1, 3, 4, 2, 7};
+        Node root = null;
+    }
+}
