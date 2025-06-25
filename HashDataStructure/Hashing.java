31c7f02ff6c2f97334f608685ef51c199335a651
BGope025 <bgope025@gmail.com>
2025-06-25 17:41:08 +0530

    adding notes question

--- HashDataStructure/Hashing.java
+++ HashDataStructure/Hashing.java
@@ -1,26 +1,44 @@
 import java.util.HashSet;
+import java.util.Iterator;
 
+// notes for hashset
 public class Hashing {
     public static void main(String[] args) {
         // creating HashSet
         HashSet<Integer> set = new HashSet<>();
 
-        // inserting data into set {.add()}
+        // inserting elements into set {.add()}
         set.add(1);
         set.add(2);
         set.add(3);
+        set.add(4);
+        set.add(5);
+        set.add(6);
         set.add(1); // adding duplicate items will get only one of them stored in set
 
-        // searching data into set {.contains()}
+        // searching element into set {.contains()}
         if (set.contains(1))
             System.out.println("present");
         else
             System.out.println("absent");
 
-        // deleting data from set {.remove()}
+        // deleting element from set {.remove()}
         set.remove(1);
         if (!set.contains(1))
             System.out.println("element removed");
 
+        // size of set
+        System.out.println("No of elements in set is..."+set.size());
+
+        // print all elements of set
+        System.out.println(set);
+
+        // iteration
+        Iterator it = set.iterator();
+
+        // 2 properties of iterator --> i) .next() ; ii) .hasNext()
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
     }
 }
