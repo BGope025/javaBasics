d42c4865628ae0970a80fcbb25413330c9e9a51a
BGope025 <bgope025@gmail.com>
2025-07-01 20:41:39 +0530

    Updating notes question

--- HashDataStructure/HashingMap.java
+++ HashDataStructure/HashingMap.java
@@ -1,4 +1,8 @@
+import com.sun.source.doctree.SeeTree;
+
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 // HashMap notes
 public class HashingMap {
@@ -25,5 +29,18 @@ public class HashingMap {
         // getting the value
         System.out.println(map.get("China")); // key exists
         System.out.println(map.get("Indonesia")); // key doesn't exist
+
+        // Iteration
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            System.out.println(e.getKey()+"\t"+e.getValue());
+        }
+        // alternative way to perform same task
+        Set<String> keys = map.keySet();
+        for (String key : keys)
+            System.out.println(key+"\t"+map.get(key));
+
+        // removing data
+        map.remove("China");
+        System.out.println(map);
     }
 }
