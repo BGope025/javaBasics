8e7ccd307074abc1be35e415ecf7f87837934e64
BGope025 <bgope025@gmail.com>
2025-07-02 20:36:10 +0530

    adding new question

--- HashDataStructure/HashMapImp1.java
+++ HashDataStructure/HashMapImp1.java
@@ -1,9 +1,8 @@
 import java.util.LinkedList;
 
-// HashMap implitation
+// HashMap implimentation
 public class HashMapImp1 {
     static class HashMap<K,V> {
-        private K key;
 
         private class Node {
             K key;
@@ -18,6 +17,7 @@ public class HashMapImp1 {
         private int N;
         private LinkedList<Node> buckets[];
 
+        @SuppressWarnings("unchecked")
         public HashMap() {
             this.N = 4;
             this.buckets =  new LinkedList[4];
@@ -33,8 +33,8 @@ public class HashMapImp1 {
         }
 
         // to search for a particular key value pair int the LinkedList stored in array
-        private int HashSearch(int N, int di) {
-            LinkedList<Node> ll = new LinkedList();
+        private int HashSearch(K key, int di) {
+            LinkedList<Node> ll = buckets[di];
             for (int i=0; i<ll.size(); i++) {
                 if (ll.get(i).key == key)
                     return i;
@@ -42,14 +42,39 @@ public class HashMapImp1 {
             return -1;
         }
 
+        // to increase the size of the array to avoid larger LinkedList
+        private void rehash() {
+            LinkedList<Node> oldBucket[] = buckets;
+            buckets = new LinkedList[N*2];
+
+            for (int i=0; i<N*2; i++) {
+                buckets[i] = new LinkedList<>();
+            }
+
+            for (int i=0; i<oldBucket.length; i++) {
+                LinkedList<Node> ll = oldBucket[i];
+                for (int j=0; j<ll.size(); j++) {
+                    Node node = ll.get(j);
+                    put(node.key, node.val);
+                }
+            }
+        }
+
+        // to check and put new data or update old data
         public void put(K key, V val) {
             int di = HashFunct(key);
-            int bi = HashSearch(N, di);
+            int bi = HashSearch(key, di);
             if (bi == -1) {
-
+                buckets[bi].add(new Node(key, val));
+                n++;
             }
             else {
-
+                Node node = buckets[bi].get(di);
+                node.val = val;
+            }
+            double lambda = (double)n/N;
+            if (lambda > 2.0) {
+                rehash();
             }
         }
 
