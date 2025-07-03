38bd6c327ff09d8a66c923c9c6cbdbae81e53f07
BGope025 <bgope025@gmail.com>
2025-07-03 08:53:27 +0530

    adding practice question

--- HashDataStructure/H3.java
+++ HashDataStructure/H3.java
@@ -1,13 +1,14 @@
 import java.util.HashSet;
 import java.util.Scanner;
 
-// wap to find the union of two sets of data
-public class H2 {
+// wap to find the intersection of two sets
+public class H3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> intersection = new HashSet<>();
 
-        // taking inputs & taking the union
+        // taking inputs
         System.out.println("enter the no of elements in both the sets");
         int n= sc.nextInt();
         int[] arr1 = new int[n];
@@ -16,15 +17,21 @@ public class H2 {
         System.out.println("enter the first set");
         for (int i=0; i<n; i++) {
             arr1[i] = sc.nextInt();
-            set.add(arr1[i]);
         }
-        System.out.println("enter the second set");
+        System.out.println("enter the first set");
         for (int i=0; i<m; i++) {
             arr2[i] = sc.nextInt();
             set.add(arr2[i]);
         }
 
-        // printing union
-        System.out.println("The union of the two sets is- \n"+set);
+        // taking intersection
+        for (int i=0; i<n; i++) {
+            if (set.contains(arr1[i]))
+                intersection.add(arr1[i]);
+        }
+
+        // printing
+        System.out.println("The intersection of two sets is- \n"+intersection);
+
     }
 }
